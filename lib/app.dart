767c338222ea66bfc75d3b80ab773a83fb33f5c1
ftreder001 <flotreder@gmail.com>
2021-02-21 02:20:28 +0100

    added web

--- lib/app.dart
+++ lib/app.dart
@@ -6,7 +6,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CDE APP',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -17,7 +17,7 @@ class App extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.red,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
